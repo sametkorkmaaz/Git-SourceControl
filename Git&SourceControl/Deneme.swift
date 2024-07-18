fb544e14563d95be1a536f068724d31abad72c82
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-07-18 15:47:44 +0300

    Deneme struct oluşturuldu

---
+++ Git&SourceControl/Deneme.swift
@@ -0,0 +1,14 @@
+//
+//  Deneme.swift
+//  Git&SourceControl
+//
+//  Created by Samet Korkmaz on 18.07.2024.
+//
+
+import Foundation
+
+struct Deneme {
+    
+    let deneme1: String
+    let deneme2: String
+}
